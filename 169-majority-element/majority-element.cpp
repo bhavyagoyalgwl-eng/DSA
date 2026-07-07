8108eb3da40449f9fef3dfe3e512f8c9afd3f43c
bhavyagoyalgwl-eng <bhavyagoyalgwl@gmail.com>
2026-07-07 21:47:25 +0530

    Time: 0 ms (100.00%) | Memory: 28.4 MB (9.85%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -3,17 +3,12 @@ public:
     int majorityElement(vector<int>& nums) {
          unordered_map<int,int>m;
          for(int i=0;i<nums.size();i++){
-            if(m.find(nums[i])==m.end()){
-                m.emplace(nums[i],1);
-
-            }
-            else{
-                m[nums[i]]++;
-            }
+                 m[nums[i]]++;
+            
 
          }
-         int freqNeeded=floor(nums.size()/2);
-        for(auto p:m){
+         int freqNeeded=nums.size()/2;
+            for(auto p:m){
             if(p.second>freqNeeded){
                 return p.first;
             }
