ff402bceb0f38997b1559218284cb5ef30d422e6
bhavyagoyalgwl-eng <bhavyagoyalgwl@gmail.com>
2026-07-14 20:00:44 +0530

    Time: 7 ms (20.63%) | Memory: 10.9 MB (70.12%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     string frequencySort(string s) {
-        map<char,int>m;
+        unordered_map<char,int>m;
         for(int i=0;i<s.size();i++){
             m[s[i]]++;
         }
