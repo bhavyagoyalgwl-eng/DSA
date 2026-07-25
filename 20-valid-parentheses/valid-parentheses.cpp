632ad83b5845ab0dfc65bc35efc2de684459a24a
bhavyagoyalgwl-eng <bhavyagoyalgwl@gmail.com>
2026-07-25 17:34:19 +0530

    Time: 3 ms (9.03%) | Memory: 10 MB (9.16%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        
+        int topofstack=0;
+        stack<char>s2;
+        int index=0;
+        
+        while(index<s.size()){
+            if(s[index]=='(' || s[index]=='{' || s[index]=='['){
+                s2.emplace(s[index]);
+                
+            }
+            if(s[index]==')'){
+            if(s2.empty() || s2.top()!='(') return false;
+            s2.pop();
+            }
+            else if(s[index]=='}'){
+            if(s2.empty() || s2.top()!='{') return false;
+            s2.pop();
+            }
+            else if(s[index]==']'){
+            if(s2.empty() || s2.top()!='[') return false;
+             s2.pop();
+            }
+            index++;
+            
+
+        }
+        return s2.empty();
+        
+    }
+};
