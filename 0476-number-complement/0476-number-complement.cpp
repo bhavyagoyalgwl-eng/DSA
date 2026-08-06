be3202a5ed3d76ceda35ea1ddfd54cf0f7c82f31
bhavyagoyalgwl-eng <bhavyagoyalgwl@gmail.com>
2026-08-07 00:53:05 +0530

    Time: 0 ms (100%), Space: 7.8 MB (97.12%) - LeetHub

---
+++ 0476-number-complement/0476-number-complement.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int findComplement(int num) {
+        if(num==0){
+            return 1;
+        }
+        int mask=0;
+        int temp=num;
+        while(temp){
+            mask=(mask<<1)|1;
+            temp>>=1;
+        }
+        return (~num)&mask;
+    }
+};
