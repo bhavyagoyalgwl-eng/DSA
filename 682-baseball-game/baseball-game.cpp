459f66b0ed7042f680470dc6d1f4eb226bde9e82
bhavyagoyalgwl-eng <bhavyagoyalgwl@gmail.com>
2026-08-07 01:40:42 +0530

    Time: 0 ms (100.00%) | Memory: 12 MB (70.96%) - LeetSync

--- 682-baseball-game/baseball-game.cpp
+++ 682-baseball-game/baseball-game.cpp
@@ -8,11 +8,11 @@ public:
                 ans.pop_back();
             }
             else if(operations[i]=="D"){
-                int x=ans[ans.size()-1];
+                int x=ans.back();
                 ans.push_back(2*x);
             }
             else if(operations[i]=="+"){
-                int x=ans[ans.size()-1];
+                int x=ans.back();
                 int y=ans[ans.size()-2];
                 
                 ans.push_back(x+y);
@@ -22,9 +22,7 @@ public:
             }
 
         }
-        if(empty(ans)){
-            return 0;
-        }
+        
         int sum=0;
         for(int i=0;i<ans.size();i++){
             sum+=ans[i];
