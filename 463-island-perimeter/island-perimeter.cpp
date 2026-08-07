ab699747c71c492895824ee01d4e805e2014cdef
bhavyagoyalgwl-eng <bhavyagoyalgwl@gmail.com>
2026-08-07 21:43:41 +0530

    Time: 3 ms (68.52%) | Memory: 101.8 MB (98.49%) - LeetSync

---
+++ 463-island-perimeter/island-perimeter.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int islandPerimeter(vector<vector<int>>& grid) {
+        int ans=0;
+        int row=grid.size();
+        int col=grid[0].size();
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                if(grid[i][j]==0) continue;
+                  if(i==0 || grid[i-1][j]==0) ans++;
+                  if(j==col-1 || grid[i][j+1]==0 ) ans++;
+                  if(i==row-1 || grid[i+1][j]==0 ) ans++;
+                  if(j==0 || grid[i][j-1]==0) ans++;
+            }
+        }
+        return ans;
+    }
+};
