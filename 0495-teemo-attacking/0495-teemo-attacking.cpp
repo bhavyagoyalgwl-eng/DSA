0e59e7367b4142ac3a7e3951329e7888e28a1356
bhavyagoyalgwl-eng <bhavyagoyalgwl@gmail.com>
2026-08-10 15:18:49 +0530

    Time: 3 ms (10.13%), Space: 29.6 MB (62.49%) - LeetHub

---
+++ 0495-teemo-attacking/0495-teemo-attacking.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int findPoisonedDuration(vector<int>& timeSeries, int duration) {
+        int ans=0;
+        for(int i=0;i<=timeSeries.size()-1;i++){
+                if((i!=timeSeries.size()-1) && (timeSeries[i]+duration-1>=timeSeries[i+1])){
+                    ans+=(timeSeries[i+1]-timeSeries[i]);
+                }
+                else{
+                    ans+=duration;
+                }
+        }
+        return ans;
+    }
+};
