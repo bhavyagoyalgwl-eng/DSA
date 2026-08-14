5f75eead34a8bc2a6045f270b52a7bef9cd901e3
bhavyagoyalgwl-eng <bhavyagoyalgwl@gmail.com>
2026-08-14 21:45:23 +0530

    Time: 105 ms (59.31%) | Memory: 92.9 MB (96.10%) - LeetSync

--- 3249-minimum-number-of-operations-to-make-array-xor-equal-to-k/minimum-number-of-operations-to-make-array-xor-equal-to-k.cpp
+++ 3249-minimum-number-of-operations-to-make-array-xor-equal-to-k/minimum-number-of-operations-to-make-array-xor-equal-to-k.cpp
@@ -5,29 +5,11 @@ public:
         for(int i=0;i<nums.size();i++){
             xor_total^=nums[i];
         }
-        int temp=k;
+        int diff=xor_total^k;
         int ans=0;
-        int d1=0;
-        int d2=0;
-        while(temp>0 || xor_total>0){
-            if(temp==0){
-                 d1=0;
-            }
-            else if(temp>0){
-                 d1=temp%2;
-            }
-            if(xor_total>0){
-                 d2=xor_total%2;
-
-            }
-            else if(xor_total==0){
-                 d2=0;
-            }
-            if(d1!=d2){
-                ans++;
-            }
-            xor_total/=2;
-            temp/=2;
+        while(diff>0){
+            diff&=diff-1;
+            ans++;
         }
         return ans;
     }
