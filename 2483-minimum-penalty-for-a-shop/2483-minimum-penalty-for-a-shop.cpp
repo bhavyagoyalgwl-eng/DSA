0457367cf62763e90d0d586b8ebf2a5251336a52
Bhavya_Goyal_123 <bhavyagoyalgwl@gmail.com>
2026-08-22 13:08:40 +0530

    Time: 7 ms (65.71%), Space: 17.7 MB (48.46%) - LeetHub

--- 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.cpp
+++ 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.cpp
@@ -8,30 +8,18 @@ public:
                 penalty++;
             }
         } 
-        
-        for(int i=0;i<=customers.size();i++){
-            if(i==0) close[i]=penalty;
-            
-            else if(customers[i-1]=='Y') penalty--;
-            close[i]=penalty;
+        int ans=0;
+        int minpenalty=penalty;
+        for(int i=1;i<=customers.size();i++){
+            if(customers[i-1]=='Y') penalty--;
+            else if(customers[i-1]=='N') penalty++;
+            if(penalty<minpenalty){
+                minpenalty=penalty;
+                ans=i;
+            } 
 
         }
-        int N=0;
-        int ans=INT_MAX;
-        for(int i=0;i<=customers.size();i++){
-            close[i]+=N;
-            if(i<customers.size() && customers[i]=='N'){
-                N++;
-                
-            }
-            ans=min(ans,close[i]);
-
-        }
-        for(int i=0;i<close.size();i++){
-            if(close[i]==ans){
-                return i;
-            }
-        }
-        return -1;
+       
+        return ans;
     }
 };
