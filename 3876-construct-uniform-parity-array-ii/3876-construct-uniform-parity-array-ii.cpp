edc80fbb3fba73c50135c9321352cbfff79157f6
Bhavya_Goyal_123 <bhavyagoyalgwl@gmail.com>
2026-09-03 15:23:24 +0530

    Time: 5 ms (54.71%), Space: 165.8 MB (87.43%) - LeetHub

---
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool uniformArray(vector<int>& nums1) {
+        int minimum=*min_element(nums1.begin(),nums1.end());
+        if(minimum%2==0){
+            
+            for(int i=0;i<nums1.size();i++){
+                if(nums1[i]%2!=0) return false;
+            }
+
+        }
+        return true;
+
+    }
+};
