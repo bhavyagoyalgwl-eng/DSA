8c98e210d0bec7d8d425310f306d62d5e6e7a540
Bhavya_Goyal_123 <bhavyagoyalgwl@gmail.com>
2026-09-04 08:27:34 +0530

    Time: 0 ms (100%), Space: 13.3 MB (87.51%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        long long ans=0;
+        for(int i=0;i<32;i++){
+             long long count=0;
+            for(int j=0;j<nums.size();j++){
+                if((nums[j]>>i)&1) count++;
+            }
+            if(count%3==1) ans|=(1<<i); 
+        }
+        return ans;
+    }
+};
