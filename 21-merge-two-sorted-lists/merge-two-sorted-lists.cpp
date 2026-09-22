96c5cefc07583cd1de94a40bc9afaa1f6888558e
Bhavya_Goyal_123 <bhavyagoyalgwl@gmail.com>
2026-09-22 21:40:53 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (62.34%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
@@ -11,29 +11,46 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        ListNode* tempA=list1;
-        ListNode* tempB=list2;
-        ListNode* c=new ListNode(100);
-        ListNode* tempC=c;
+        if(list1==NULL){
+            if(list2==NULL ) return NULL;
+            else return list2;
+        }
+        else if(list2==NULL){
+            if(list1==NULL) return NULL;
+            else return list1;
+        }
+        ListNode*tempA=list1;
+        ListNode*tempB=list2;
+        ListNode*c=new ListNode(100);
+        ListNode*tempC=c;
         while(tempA!=NULL && tempB!=NULL){
+        if(tempA->val>=tempB->val){
+            tempC->next=tempB;
+            tempC=tempB;
+            if(tempB->next==NULL) break;
+            tempB=tempB->next;
+            
+        }
+        else if(tempB->val>tempA->val){
+            tempC->next=tempA;
+            tempC=tempA;
+            if(tempA->next==NULL) break;
+            tempA=tempA->next;
+        }
+        }
+        if(tempA->next==NULL && tempB->next==NULL){
             if(tempA->val>=tempB->val){
-                ListNode*t=new ListNode(tempB->val);
-                tempC->next=t;
-                tempC=t;
-                tempB=tempB->next;
+                tempB->next=tempA;
             }
             else{
-                ListNode*t=new ListNode(tempA->val);
-                tempC->next=t;
-                tempC=t;
-                tempA=tempA->next;
+                tempA->next=tempB;
             }
-        }   
-        if(tempA!=NULL){
-            tempC->next=tempA;
         }
-        else if(tempB!=NULL){
+        else if(tempA->next==NULL){
             tempC->next=tempB;
+        }
+        else{
+            tempC->next=tempA;
         }
         return c->next;
     }
